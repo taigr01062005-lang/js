e66a8396462334d2eb51a15b78da5b670d9e98e1
H82M19\Student <Student>
2026-01-26 06:46:50 +0700

    2

--- tuan02/src/App.jsx
+++ tuan02/src/App.jsx
@@ -4,13 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { ProductCard } from '../components/ProductCard'
 import { Button } from '../components/Button'
+import Alert from '../components/Alert'
+import Input from '../components/Input'
+import LoginForm from '../components/LoginForm'
+import ProductList from '../components/ProductList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let [alertStatus,setAlertStatus]=useState(null)
   return (
-    <div>
-      <ProductCard></ProductCard>
+    <div className='App'>
+      <ProductList></ProductList>
+     
     </div>
   )
 }
